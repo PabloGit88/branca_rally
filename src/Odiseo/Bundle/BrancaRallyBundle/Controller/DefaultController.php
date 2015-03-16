15bd6d0dc882843be58bdd347356f6a729239e9d
Pablo Damico <pablo.damico88@gmail.com>
2015-03-16 00:36:44 -0300

    init

---
+++ src/Odiseo/Bundle/BrancaRallyBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Odiseo\Bundle\BrancaRallyBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('OdiseoBrancaRallyBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Odiseo/Bundle/BrancaRallyBundle/OdiseoBrancaRallyBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Odiseo\Bundle\BrancaRallyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OdiseoBrancaRallyBundle extends Bundle
+{
+}
